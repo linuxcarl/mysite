edc150f77e43a53bdc4e588762753ab98db471a6
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-06-01 00:15:11 -0500

    add functions __str__ in models and activated manage in admin panel por polls

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,27 @@
+# https://docs.djangoproject.com/es/3.0/intro/tutorial02/
+import datetime
 from django.db import models
+from django.utils import timezone
 # Create your models here.
-#Has a quiestion and a publicarion date 
+
+# Has a quiestion and a publicarion date
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-#Has two fields, the text of the choice and a vote tally
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+# Has two fields, the text of the choice and a vote tally
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
